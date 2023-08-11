f706ef0dbea05dff1e8a11f0ea1a0b21b514f142
Renato Bueno <renatobdo@gmail.com>
2023-08-11 19:35:57 -0300

    Atualizacao de conteúdo
    Atualizacao de conteúdo

---
+++ Semana3/formulario_carro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo de Formulário do carro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css\styles.css">
+</head>
+<body>
+    <div class="container">
+        <h3>Preencha os Dados</h3>
+        <form method="post">
+            <div class="form-group">
+                <label for="marca">marca:</label>
+                <input type="text" class="form-control" id="marca" name="marca" required>
+            </div>
+            <div class="form-group">
+                <label for="modelo">Modelo:</label>
+                <input type="text" class="form-control" id="modelo" name="modelo" required>
+            </div>
+            <div class="form-group">
+                <label for="ano">Ano:</label>
+                <input type="date" class="form-control" id="ano" name="ano" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+</div>
+</body>
+</html>
